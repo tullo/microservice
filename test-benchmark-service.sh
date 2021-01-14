c9d0ca56b3b5d9e4f69a2a726efd3d150eac5e64
Andreas Amstutz <tullo@users.noreply.github.com>
2021-01-14 14:40:17 +0100

    add load test for haberdasher service

--- test-benchmark-service.sh
+++ test-benchmark-service.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+echo
+echo "============================================================================+"
+echo "=== Stats Service                                                           |"
+echo "============================================================================+"
+echo
+
 function get_stats_ip {
 	docker inspect \
 		microservice_stats_1 | \
@@ -9,5 +15,24 @@ url="http://$(get_stats_ip):3000/twirp/stats.StatsService/Push"
 image="skandyla/wrk:latest"
 
 docker run --rm --net=host -it -v $PWD:/data $image -d60s -t4 -c100 -s test.lua $url
-
 #docker run --rm --net=host -it -v $PWD:/data $image -d15s -t4 -c400 -s test.lua $url
+
+echo
+echo "============================================================================+"
+echo "=== Haberdasher Service                                                     |"
+echo "============================================================================+"
+echo
+
+function get_hd_ip {
+	docker inspect \
+		microservice_hd_1 | jq -r '.[0].NetworkSettings.Networks["microservice_default"]'.IPAddress
+}
+
+url="http://$(get_hd_ip):3000/twirp/haberdasher.HaberdasherService/MakeHat"
+$(go env GOPATH)/bin/hey \
+	-h2 \
+	-T 'application/json' \
+	-d '{"centimeters": 59}' \
+	-m POST \
+	-z 15s \
+	$url
