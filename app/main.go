75e7b885e6259fd3c7ef561630b337e9092e61c0
Andreas Amstutz <tullo@users.noreply.github.com>
2021-01-15 16:45:43 +0100

    use protobuf service client

--- app/main.go
+++ app/main.go
@@ -4,16 +4,16 @@ import (
 	"context"
 	"log"
 
-	"github.com/tullo/microservice/client/haberdasher"
+	_ "github.com/tullo/microservice/client/haberdasher"
 	"github.com/tullo/microservice/inject"
-	rp "github.com/tullo/microservice/rpc/haberdasher"
+	pb "github.com/tullo/microservice/rpc/haberdasher"
 )
 
 func main() {
 	// hd := haberdasher.New()
-	// hd := haberdasher.NewHaberdasherServiceProtobufClient("http://172.28.0.4:3000", inject.NewHTTPClient())
-	hd := haberdasher.NewCustom("http://172.28.0.4:3000", inject.NewHTTPClient())
-	hat, err := hd.MakeHat(context.Background(), &rp.Size{Centimeters: 78})
+	// hd := haberdasher.NewCustom("http://172.28.0.4:3000", inject.NewHTTPClient())
+	hd := pb.NewHaberdasherServiceProtobufClient("http://172.28.0.4:3000", inject.NewHTTPClient())
+	hat, err := hd.MakeHat(context.Background(), &pb.Size{Centimeters: 58})
 	if err != nil {
 		panic(err)
 	}
